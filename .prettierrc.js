1166684a483322a5d0a6c437320dd47669bf6465
shdulu <17625329975@163.com>
2022-11-30 22:00:37 +0800

    feat：命令注册

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,12 @@
 /** @format */
 
 module.exports = {
-  printWidth: 120, // 超过最大值换行
+  printWidth: 100, // 超过最大值换行
   tabWidth: 2, // 缩进字节数
   useTabs: false, // 缩进不使用tab，使用空格
-  semi: false, // 句尾添加分号
+  semi: true, // 句尾添加分号
   singleQuote: true, // 使用单引号代替双引号
-  proseWrap: 'preserve', // 默认值。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
+  // proseWrap: 'preserve', // 默认值。因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
   arrowParens: 'avoid', //  (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid：省略括号
   bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
   endOfLine: 'auto', // 结尾是 \n \r \n\r auto
--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,111 +1,143 @@
 /** @format */
 
-'use strict'
-const path = require('path')
-const semver = require('semver')
-const colors = require('colors/safe')
-const userHome = require('user-home')
-const pathExists = require('path-exists').sync
-const pkg = require('../package.json')
-const log = require('@swin-cli/log')
-const constant = require('./const')
-let args
-
+'use strict';
+const path = require('path');
+const semver = require('semver');
+const colors = require('colors/safe');
+const userHome = require('user-home');
+const pathExists = require('path-exists').sync;
+const pkg = require('../package.json');
+const log = require('@swin-cli/log');
+const commander = require('commander');
+const constant = require('./const');
+let args;
+const program = new commander.Command();
 async function core() {
   // 1.检查版本号 -> 2.检查node版本 -> 3.检查root启动 -> 4.检查用户主目录
   // 5.检查入参 -> 6.检查环境变量 -> 7.检查是否为最新版本 -> 8.提示更新
   try {
-    debugger
-    checkPkgVersion()
-    checkNodeVersion()
-    checkRoot()
-    checkUserHome()
-    checkInputArgs()
-    checkEnv()
-    await checkGlobalUpdate()
-    // --debug 开启模式打印debug log
-    log.verbose('debug', 'test debug log')
+    checkPkgVersion();
+    checkNodeVersion();
+    checkRoot();
+    checkUserHome();
+    // checkInputArgs();
+    checkEnv();
+    await checkGlobalUpdate();
+    registerCommand();
   } catch (error) {
-    log.error(error.message)
+    log.error(error.message);
   }
 }
 
 // 1. 检查版本号
 function checkPkgVersion() {
-  log.notice('cli', pkg.version)
+  log.notice('cli', pkg.version);
 }
 
 // 2. 检查 node 版本
 function checkNodeVersion() {
   // 第一步，获取当前node版本号
-  const currentVersion = process.version
+  const currentVersion = process.version;
   //第二步，比对最低版本号
-  const lowestNodeVersion = constant.LOWEST_NODE_VERSION
+  const lowestNodeVersion = constant.LOWEST_NODE_VERSION;
   if (!semver.gte(currentVersion, lowestNodeVersion)) {
-    throw new Error(colors.red(`swin-cli 需要安装 v${lowestNodeVersion} 以上版本的 Node.js`))
+    throw new Error(colors.red(`swin-cli 需要安装 v${lowestNodeVersion} 以上版本的 Node.js`));
   }
 }
 // 3. 检查 root 账户管理员启动 降级处理
 function checkRoot() {
-  const rootCheck = require('root-check')
-  rootCheck()
+  const rootCheck = require('root-check');
+  rootCheck();
 }
 // 4.检查用户主目录
 function checkUserHome() {
   if (!userHome || !pathExists(userHome)) {
-    throw new Error(colors.red('当前登录用户主目录不存在！'))
+    throw new Error(colors.red('当前登录用户主目录不存在！'));
   }
 }
 // 5.检查入参
 function checkInputArgs() {
-  const minimist = require('minimist')
-  args = minimist(process.argv.slice(2))
-  checkArgs()
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  checkArgs();
 }
 function checkArgs() {
-  process.env.LOG_LEVEL = args.debug ? 'verbose' : 'info'
-  log.level = process.env.LOG_LEVEL
+  process.env.LOG_LEVEL = args.debug ? 'verbose' : 'info';
+  log.level = process.env.LOG_LEVEL;
 }
 // 6.检查环境变量
 function checkEnv() {
-  const dotenv = require('dotenv')
+  const dotenv = require('dotenv');
   // Default: path.resolve(process.cwd(), '.env')
-  let dotenvPath = path.resolve(userHome, '.env')
+  let dotenvPath = path.resolve(userHome, '.env');
   if (pathExists(dotenvPath)) {
     dotenv.config({
       path: dotenvPath,
-    })
+    });
   }
-  createDefaultConfig()
+  createDefaultConfig();
 }
 function createDefaultConfig() {
   const cliConfig = {
     home: userHome,
-  }
+  };
   if (process.env.CLI_HOME) {
-    cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME)
+    cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME);
   } else {
-    cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME)
+    cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME);
   }
   // 设置 cli 缓存目录
-  process.env.CLI_HOME_PATH = cliConfig.cliHome
+  process.env.CLI_HOME_PATH = cliConfig.cliHome;
 }
 // 7.检查是否为最新版本 & 8.提示更新
 async function checkGlobalUpdate() {
   // 1. 获取当前版本号和模块名
-  const currentVersion = pkg.version
-  const npmName = pkg.name
+  const currentVersion = pkg.version;
+  const npmName = pkg.name;
   // 2. 调用 npm API， 获取所有版本号
-  const { genNpmSemverVersion } = require('@swin-cli/get-npm-info')
-  const lastVersion = await genNpmSemverVersion(currentVersion, npmName)
+  const { genNpmSemverVersion } = require('@swin-cli/get-npm-info');
+  const lastVersion = await genNpmSemverVersion(currentVersion, npmName);
   if (lastVersion && semver.gt(lastVersion, currentVersion)) {
     log.warn(
       colors.yellow(
         `请手动更新 ${npmName}, 当前版本：${currentVersion}, 最新版本: ${lastVersion} 更新命令：npm install -g ${npmName}`
       )
-    )
+    );
   }
   // 3. 提取所有版本号，比对那些版本号是大于当前版本号
   // 4. 获取最新的版本号，提示用户更新到该版本
 }
-module.exports = core
+function registerCommand() {
+  program
+    .name(Object.keys(pkg.bin)[0])
+    .usage('<command> [options]')
+    .version(pkg.version)
+    .option('-d, --debug', '是否开启调试模式', false);
+
+  program
+    .command('init [projectName]')
+    .option('-f, --force', '是否强制初始化项目', false)
+    .action((projectName, cmdObj) => {
+      console.log('init', projectName, cmdObj.force);
+    });
+
+  // 开启debug模式
+  program.on('option:debug', () => {
+    process.env.LOG_LEVEL = program.debug ? 'verbose' : 'info';
+    log.level = process.env.LOG_LEVEL;
+  });
+  // 监听未注册的命令
+  program.on('command:*', obj => {
+    const availableCommands = program.commands.map(cmd => cmd.name());
+    console.log(colors.red('未知命令：' + obj[0]));
+    if (availableCommands.length > 0) {
+      console.log(colors.red('可用命令：' + availableCommands.join(',')));
+    }
+  });
+  program.parse(process.argv);
+  if (program.args && program.args.length < 1) {
+    // 没有指定命令 - 打印帮助文档
+    program.outputHelp();
+  }
+}
+module.exports = core;
